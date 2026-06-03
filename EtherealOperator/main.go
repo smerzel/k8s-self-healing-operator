e6c1220c2506f983b0c6d9d853d36e999fc388ab
saralemerz <saralemerz@Gmail.com>
2026-06-04 00:54:27 +0300

    Update operator to polling architecture for stability

--- EtherealOperator/main.go
+++ EtherealOperator/main.go
@@ -10,34 +10,33 @@ import (
 
 	"github.com/redis/go-redis/v9"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/util/workqueue" // <--- הנתיב המתוקן נמצא כאן
 )
 
 type Controller struct {
 	k8sClient   *kubernetes.Clientset
 	redisClient *redis.Client
-	queue       workqueue.RateLimitingInterface
 	logger      *slog.Logger
 }
 
 func (c *Controller) Reconcile() {
 	ctx := context.Background()
+	// בדיקת גודל התור
 	load, err := c.redisClient.LLen(ctx, "sunday_tasks_queue").Result()
 	if err != nil {
 		c.logger.Error("Failed to check Redis load", "error", err)
 		return
 	}
 
+	// קבלת מצב ה-Worker
 	scale, err := c.k8sClient.AppsV1().Deployments("default").GetScale(ctx, "sunday-worker", metav1.GetOptions{})
 	if err != nil {
 		c.logger.Error("Failed to get deployment scale", "error", err)
 		return
 	}
 
+	// לוגיקת Scaling
 	if load > 15 && scale.Spec.Replicas < 3 {
 		scale.Spec.Replicas = 3
 		_, err = c.k8sClient.AppsV1().Deployments("default").UpdateScale(ctx, "sunday-worker", scale, metav1.UpdateOptions{})
@@ -53,46 +52,27 @@ func main() {
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
 	config, _ := rest.InClusterConfig()
 	k8sClient, _ := kubernetes.NewForConfig(config)
-	
+
 	c := &Controller{
 		k8sClient:   k8sClient,
 		redisClient: redis.NewClient(&redis.Options{Addr: "redis-service:6379"}),
 		logger:      logger,
-		queue:       workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "etherealpods"),
 	}
 
-	factory := informers.NewSharedInformerFactory(k8sClient, time.Minute*10)
-	deploymentInformer := factory.Apps().V1().Deployments().Informer()
+	logger.Info("Operator ready (Polling Architecture)")
 
-	deploymentInformer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-		UpdateFunc: func(oldObj, newObj interface{}) {
-			c.queue.Add("reconcile-event")
-		},
-	})
-
-	stopCh := make(chan struct{})
-	factory.Start(stopCh)
-	
-	pubsub := c.redisClient.Subscribe(context.Background(), "task_events")
-	go func() {
-		for range pubsub.Channel() {
-			c.queue.Add("task-event")
-		}
-	}()
-
-	logger.Info("Operator ready (Full Event-Driven Architecture)")
-
-	go func() {
-		for {
-			obj, _ := c.queue.Get()
-			c.Reconcile()
-			c.queue.Done(obj)
-		}
-	}()
-
+	// לולאת עבודה - סריקה כל 5 שניות
+	ticker := time.NewTicker(5 * time.Second)
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	close(stopCh)
-	logger.Info("Shutting down gracefully")
+
+	for {
+		select {
+		case <-ticker.C:
+			c.Reconcile()
+		case <-quit:
+			logger.Info("Shutting down gracefully")
+			return
+		}
+	}
 }
